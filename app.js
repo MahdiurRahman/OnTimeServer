2e6beec889b32d4e9e1e2f845980e0d1d22386ce
Mahdiur Rahman <rmahdiur@gmail.com>
2020-02-15 22:25:09 -0500

    closes #2: restructures file format

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
 const express = require("express");
-const mysql = require("mysql");
-require("dotenv").config();
+const db = require("./database/connection");
 
-let app = express();
-
-let config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST
-};
-
-const db = mysql.createConnection(config);
-db.connect(function(err) {
-    if (err) {
-        console.error("FAILURE: Error connecting to db: " + err.stack);
-        return;
-    }
-    console.log("SUCCESS: Connected to db as thread id: " + db.threadId);
-/*      const result = db.query("DESC test", (er, res, fields) => {
-        console.log(res);
-    });  */
-});
-module.exports = db;
+const app = express();
 
 app.listen(3000, () => {
     console.log("Server started");
