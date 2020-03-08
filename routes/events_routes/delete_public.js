a62eb9c4ef620ee1c8d655f8e417a70297050697
Mahdiur Rahman <rmahdiur@gmail.com>
2020-03-07 21:55:47 -0500

    closes #23 - adds EDIT private and public routes (NOTE: not tested)

---
+++ routes/events_routes/delete_public.js
@@ -0,0 +1,78 @@
+const { query } = require("../../database/connection");
+const delete_public = require("express").Router();
+
+delete_public.put("/", async (req, res) => {
+    // 1. find event
+    let event
+    try {
+        event = await query(`SELECT * FROM events_private WHERE id=${body.id}`)
+    } catch (error) {
+        res.send(error)
+    }
+
+    // 2. check code
+    if (body.code != event.code) {
+        res.send(`Sorry. Wrong credentials to delete event`)
+    }
+
+    // 3. delete event
+    let delete_event
+    try {
+        delete_event = await query(`DELETE FROM events_private WHERE id=${body.id}`)
+    } catch (error) {
+        res.send(error)
+    }
+
+    // 4. get all users connected to this event (NOT including owner)
+    let users
+    try {
+        users = await query(`SELECT * FROM users_to_public 
+        WHERE 
+                eventId=${body.id}
+            AND
+                userId!=${event.ownerId}`)
+    } catch (error) {
+        res.send(error)
+    }
+
+    // 5. propogate notification
+    let current_time
+    let message = `The event '${event.eventName}' was deleted.`  
+    let notification
+    for (let i = 0; i < users.length; i++) {
+        current_time = new Date();
+        current_time = current_time.toJSON().replace("T", " ").slice(0, -5);
+        try {
+            notification = await query(`INSERT INTO notifications (
+                userId, 
+                eventId, 
+                createdOn, 
+                message)
+            VALUES
+                ${users[i].userId},
+                ${event.id},
+                '${current_time}',
+                '${message}'`)
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    // 6. delete all entrys in users_to_public
+    let delete_users_to_public
+    try {
+        delete_users_to_private = await query(`DELETE FROM users_to_public WHERE eventId=${body.id}`)
+    } catch(error) {
+        res.send(error)
+    }
+
+    res.send({
+        ...req.body,
+        event, 
+        delete_event,
+        notification,
+        delete_users_to_public,
+    })
+})
+
+module.exports = delete_public
